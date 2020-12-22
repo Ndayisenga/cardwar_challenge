2eb0568f98e2c328d7c618172c8196464568254c
John <cloude056@yahoo.fr>
2020-12-22 19:06:09 +0200

    Initial Commit

---
+++ cardwar_challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardwar_challenge
+//
+//  Created by Jean Claude Ndayisenga on 22/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
